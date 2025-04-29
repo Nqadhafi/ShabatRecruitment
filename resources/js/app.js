d69063122a5aa46309036b5f3181688c6670c8fc
Nqadhafi <qadhafi817@gmail.com>
2025-04-29 13:29:01 +0700

    Client side vue inertia instalation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+ import { createApp, h } from 'vue';
+ import { App, plugin } from '@inertiajs/inertia-vue3';
+
+ const el = document.getElementById('app');
+
+ createApp({
+    redner: ()=> h(App,{
+        initialPage: JSON.parse(el.dataset.page),
+        resolveComponent: name => require(`./Pages/${name}`),
+    })
+ }).use(plugin).mount(el);
