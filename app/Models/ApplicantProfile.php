a8092ca59581183572530a0d2290ba1790063ebe
Nqadhafi <qadhafi817@gmail.com>
2025-05-28 08:57:05 +0700

    add relation model between aplicant profile to user

--- app/Models/ApplicantProfile.php
+++ app/Models/ApplicantProfile.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Education;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class ApplicantProfile extends Model
 {
@@ -22,4 +23,20 @@ class ApplicantProfile extends Model
     {
         return $this->belongsTo(Education::class, 'education_uuid');
     }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'id', 'profiles_uuid');
+    }
+
+        protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($applicantProfile) {
+            if (!$applicantProfile->id) {
+                $applicantProfile->id = (string) Str::uuid(); // Menetapkan UUID saat pembuatan data
+            }
+        });
+        }
+
 }
