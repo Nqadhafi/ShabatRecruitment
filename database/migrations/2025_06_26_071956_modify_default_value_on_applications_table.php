1a77c78e887baef1e187c3d92ad6117ddacbab0f
Nqadhafi <qadhafi817@gmail.com>
2025-06-26 14:31:04 +0700

    make migration to applications table

---
+++ database/migrations/2025_06_26_071956_modify_default_value_on_applications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyDefaultValueOnApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('applications', function (Blueprint $table) {
+                    $table->enum('status', ['applied', 'processed', 'rejected', 'hired'])
+                  ->default('applied')
+                  ->after('applicant_profile_id'); // Ganti sesuai posisi yang kamu inginkan
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+    }
+}
