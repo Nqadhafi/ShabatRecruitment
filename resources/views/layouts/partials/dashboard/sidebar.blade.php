9c3737cb2038f0854913aff688b9f5fc5820cc66
Nqadhafi <qadhafi817@gmail.com>
2025-06-30 11:57:39 +0700

    add logo sidebar

--- resources/views/layouts/partials/dashboard/sidebar.blade.php
+++ resources/views/layouts/partials/dashboard/sidebar.blade.php
@@ -3,11 +3,13 @@
                     @if(Auth::user()->role == 'admin')
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <a href="#" class="brand-link">
+            <img src="{{ asset('app/img/Logo_square.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Portal HRD</span>
     </a>
                     @elseif(Auth::user()->role == 'applicant')
 <aside class="main-sidebar sidebar-light-primary elevation-4">
         <a href="#" class="brand-link">
+            <img src="{{ asset('app/img/Logo_square.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Portal Pelamar</span>
     </a>
                     @endif
