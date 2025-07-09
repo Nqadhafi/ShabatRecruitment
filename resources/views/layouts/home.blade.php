600644343cb5e880ea88b4caf72147c4e6d92936
Nqadhafi <qadhafi817@gmail.com>
2025-07-09 09:58:34 +0700

    add meta tag for seo

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,39 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="title" content="E-Recuitment - Shabat Printing" />
+    <meta name="description"
+        content="Bergabunglah dengan digital printing & offset printing terbaik di Kota Solo. Mulai perjalanan karirmu di Shabat Printing." />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://career.shabatprinting.com/" />
+    <meta property="og:title" content="E-Recuitment - Shabat Printing" />
+    <meta property="og:description"
+        content="Bergabunglah dengan digital printing & offset printing terbaik di Kota Solo. Mulai perjalanan karirmu di Shabat Printing." />
+    <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+    <!-- X (Twitter) -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://career.shabatprinting.com/" />
+    <meta property="twitter:title" content="E-Recuitment - Shabat Printing" />
+    <meta property="twitter:description"
+        content="Bergabunglah dengan digital printing & offset printing terbaik di Kota Solo. Mulai perjalanan karirmu di Shabat Printing." />
+    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+    <!-- Meta Tags Generated with https://metatags.io -->
     <link rel="stylesheet" href="{{ asset('adminlte/css/adminlte.min.css') }}">
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{asset ('app/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('app/main.css') }}">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
     @stack('home-css')
     <link rel="icon" href="{{ asset('app/img/Logo_square.png') }}" type="image/png">
     <title>@yield('title', 'E-Recuitment - Shabat Printing')</title>
+    
 </head>
+
 <body>
 
-     <!-- Navbar -->
-@include('layouts.partials.home.navbar')
+    <!-- Navbar -->
+    @include('layouts.partials.home.navbar')
     <!-- Main Content -->
-<div class="container-fluid m-0 p-0">
-@yield('home-content')
-    <!-- Footer -->
-@include('layouts.partials.home.footer')
-</div>
+    <div class="container-fluid m-0 p-0">
+        @yield('home-content')
+        <!-- Footer -->
+        @include('layouts.partials.home.footer')
+    </div>
 </body>
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('adminlte/js/adminlte.min.js') }}"></script>
-<script src="{{ asset('app/scrolltop.js')}}"></script>
+<script src="{{ asset('app/scrolltop.js') }}"></script>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 @stack('home-scripts')
+
 </html>
